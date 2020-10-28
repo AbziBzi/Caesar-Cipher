e0e1d11df0a3fdb27d578def0abdcc37b3d1c932
Edvinas Zubavicius <edwin.zubowicz@gmail.com>
2020-10-28 20:12:19 +0200

    finished homework

---
+++ Caesar-Cipher/CaesarCipher.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Caesar_Cipher
+{
+    public class CaesarCipher
+    {
+        public string GetTextFromUser(TextReader reader)
+        {
+            Regex r = new Regex("^[a-zA-Z ]+$");
+            string input = reader.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                return null;
+            if (!r.IsMatch(input))
+                return null;
+            return input;
+        }
+
+        public int GetShiftFromUser(TextReader reader)
+        {
+            string input = reader.ReadLine();
+            if (!Int32.TryParse(input, out var shift))
+                return 0;
+            return shift;
+        }
+
+        public string Encrypt(string plainText, int shift)
+        {
+            string cipherText = string.Empty;
+
+            foreach (var c in plainText)
+            {
+                char? changedChar = CipherAlgorithm(c, shift);
+                if (changedChar == null)
+                    return null;
+                cipherText += changedChar;
+            }
+            return cipherText;
+        }
+
+        public static char? CipherAlgorithm(char c, int shift)
+        {
+            if (!char.IsLetter(c) && c != 32)
+                return null;
+            if (c == 32)
+                return c;
+            char offset = char.IsUpper(c) ? 'A' : 'a';
+            return (char) ((c + shift - offset) % 26 + offset);
+        }
+
+        public string Decrypt(string encryptedText, int shift)
+        {
+            return Encrypt(encryptedText, 26 - shift);
+        }
+
+        public static void Main()
+        {
+            CaesarCipher cipher = new CaesarCipher();
+
+            // Get text to encrypt from user input
+            Console.WriteLine("Write text below you want to encrypt:");
+            string text = cipher.GetTextFromUser(Console.In);
+            if (text == null)
+            {
+                Console.WriteLine("Input is invalid");
+                Environment.Exit(0);
+            }
+
+            // Get shift for encryption from user input
+            Console.WriteLine("Write shift for encryption algorithm");
+            int shift = cipher.GetShiftFromUser(Console.In);
+            if (shift == 0)
+            {
+                Console.WriteLine("Input is invalid");
+                Environment.Exit(0);
+            }
+
+            // Encrypt plain text
+            string cipherText = cipher.Encrypt(text, shift);
+            if (cipherText == null)
+            {
+                Console.WriteLine("Encryption has failed");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"Cipher text:\n'{cipherText}'");
+            
+            // Decrypt cipher
+            string decryptedText = cipher.Decrypt(cipherText, shift);
+            if (decryptedText == null)
+            {
+                Console.WriteLine("Decryption has failed");
+                Environment.Exit(0);
+            }
+            Console.WriteLine($"Decrypted text:\n'{decryptedText}'");
+        }
+    }
+}
---
+++ Caesar-CipherTests/CaesarCipherTests.cs
@@ -0,0 +1,326 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Caesar_Cipher.UnitTests
+{
+    [TestClass]
+    public class CaesarCipherTests
+    {
+        [TestMethod]
+        public void GetTextFromUserTest_WithValidInput()
+        {
+            // Arrange
+            const string input = "ABC";
+            var stringReader = new StringReader(input);
+            const string expected = "ABC";
+            CaesarCipher cipher = new CaesarCipher();
+            
+            // Act 
+            string gottenValue = cipher.GetTextFromUser(stringReader);
+
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetTextFromUserTest_WhenInputIsEmptyString()
+        {
+            // Arrange
+            const string input = "";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+
+            // Act 
+            string gottenValue = cipher.GetTextFromUser(stringReader);
+
+            // Assert
+            Assert.IsNull(gottenValue);
+        }
+
+        [TestMethod]
+        public void GetTextFromUserTest_WithNumbersInInput()
+        {
+            // Arrange
+            const string input = "ABC1";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+
+            // Act 
+            string gottenValue = cipher.GetTextFromUser(stringReader);
+
+            // Assert
+            Assert.IsNull(gottenValue);
+        }
+
+        [TestMethod]
+        public void GetTextFromUserTest_WithSpaceInInput()
+        {
+            // Arrange
+            const string input = "ABC DEF";
+            var stringReader = new StringReader(input);
+            const string expected = "ABC DEF";
+            CaesarCipher cipher = new CaesarCipher();
+
+            // Act 
+            string gottenValue = cipher.GetTextFromUser(stringReader);
+
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetTextFromUserTest_WhenInputIsNotEqual()
+        {
+            // Arrange
+            const string input = "ABC DEF";
+            var stringReader = new StringReader(input);
+            const string expected = "DEF ABC";
+            CaesarCipher cipher = new CaesarCipher();
+
+            // Act 
+            string gottenValue = cipher.GetTextFromUser(stringReader);
+
+            // Assert
+            Assert.AreNotEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WithValidInput()
+        {
+            // Arrange
+            const string input = "1";
+            var stringReader = new StringReader(input);
+            const int expected = 1;
+            CaesarCipher cipher = new CaesarCipher();
+            
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WithEmptyInput()
+        {
+            // Arrange
+            const string input = "";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 0;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenInputIsWithFraction()
+        {
+            // Arrange
+            const string input = "1.2";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 0;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenInputIsNotEqual()
+        {
+            // Arrange
+            const string input = "1";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 2;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+
+            // Assert
+            Assert.AreNotEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenInputIsNegative()
+        {
+            // Arrange
+            const string input = "-1";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = -1;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenIsOnlyDash()
+        {
+            // Arrange
+            const string input = "-";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 0;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenSpaceAtTheEndInput()
+        {
+            // Arrange
+            const string input = "12 ";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 12;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+            //Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+
+        [TestMethod]
+        public void GetShiftFromUserTest_WhenSpaceInBetween()
+        {
+            // Arrange
+            const string input = "12 1";
+            var stringReader = new StringReader(input);
+            CaesarCipher cipher = new CaesarCipher();
+            const int expected = 0;
+            // Act
+            int gottenValue = cipher.GetShiftFromUser(stringReader);
+            // Assert
+            Assert.AreEqual(expected, gottenValue);
+        }
+        [TestMethod]
+        public void EncryptTest_WhenGettingUpperLetters()
+        {
+            // Arrange
+            const string text = "ABC";
+            const int shift = 2;
+            const string expected = "CDE";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenGettingLowerLetters()
+        {
+            // Arrange
+            const string text = "abc";
+            const int shift = 2;
+            const string expected = "cde";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenGettingLowerAndUpperLetters()
+        {
+            // Arrange
+            const string text = "AbCd";
+            const int shift = 2;
+            const string expected = "CdEf";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenInInputIsDigit()
+        {
+            // Arrange
+            const string text = "Ab1d";
+            const int shift = 2;
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.IsNull(encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenInInputIsSpace()
+        {
+            // Arrange
+            const string text = "Ab Cd";
+            const int shift = 2;
+            const string expected = "Cd Ef";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenResultIsWrong()
+        {
+            // Arrange
+            const string text = "Ab Cd";
+            const int shift = 2;
+            const string expected = "aa aa";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreNotEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void EncryptTest_WhenShiftIsNegative()
+        {
+            // Arrange
+            const string text = "ABC";
+            const int shift = -2;
+            const string expected = "ZAB";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Encrypt(text, shift);
+            // Assert
+            Assert.AreNotEqual(expected, encryptedText);
+        }
+        [TestMethod]
+        public void DecryptTest_WithValidData()
+        {
+            // Arrange
+            const string text = "CC dd";
+            const int shift = 2;
+            const string expected = "AA bb";
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Decrypt(text, shift);
+            // Assert
+            Assert.AreEqual(expected, encryptedText);
+        }
+
+        [TestMethod]
+        public void DecryptTest_WithInvalidData()
+        {
+            // Arrange
+            const string text = "CC dd12";
+            const int shift = 2;
+            CaesarCipher cipher = new CaesarCipher();
+            // Act
+            string encryptedText = cipher.Decrypt(text, shift);
+            // Assert
+            Assert.IsNull(encryptedText);
+        }
+    }
+}
